af649b2d54ec2fabe1b913b8d29422aabfcedf41
JustmrMax <maxmain_161_g@mail.ru>
2024-08-24 09:06:07 -0700

    Logging system is Created!

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -4,6 +4,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int* args, int** argv)
 {
+	//Init Logging system
+	Hazel::Log::Init();
+
+	HZ_CORE_WARN("Initialize log!");
+	HZ_INFO("App");
+
 	auto application = Hazel::CreateApplication();
 	application->Run();
 	delete application;
